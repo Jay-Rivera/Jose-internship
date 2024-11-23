b62dd7f226335baea59b5ede65860c850f9e275a
Jay-Rivera <119644467+Jay-Rivera@users.noreply.github.com>
2024-11-22 16:13:16 -0800

    Fully Integrated the Author Page as well as the author Items component

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link, useParams } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
 import axios from "axios";
 import Skeleton from "../components/UI/Skeleton";
 
@@ -44,70 +43,104 @@ const Author = () => {
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
         <div id="top"></div>
-        {isLoading === false ? (
-          <>
-            <section
-              id="profile_banner"
-              aria-label="section"
-              className="text-light"
-              data-bgimage="url(images/author_banner.jpg) top"
-              style={{ background: `url(${AuthorBanner}) top` }}
-            ></section>
-          </>
-        ) : (
-          <>
-            <section>
-              <Skeleton width="100%" height="400px" />
-            </section>
-          </>
-        )}
+        <section
+          id="profile_banner"
+          aria-label="section"
+          className="text-light"
+          data-bgimage="url(images/author_banner.jpg) top"
+          style={{ background: `url(${AuthorBanner}) top` }}
+        ></section>
 
         <section aria-label="section">
           <div className="container">
             <div className="row">
               <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={authorData.authorImage} alt="" />
-
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          {authorData.authorName}
-                          <span className="profile_username">
-                            @{authorData.tag}
-                          </span>
-                          <span id="wallet" className="profile_wallet">
-                            {authorData.address}
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="profile_follow de-flex">
+                {!isLoading ? (
+                  <div className="d_profile de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">
-                        {totalFollowers} followers
+                      <div className="profile_avatar">
+                        <img src={authorData.authorImage} alt="" />
+
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            {authorData.authorName}
+                            <span className="profile_username">
+                              @{authorData.tag}
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              {authorData.address}
+                            </span>
+                            <button id="btn_copy" title="Copy Text">
+                              Copy
+                            </button>
+                          </h4>
+                        </div>
                       </div>
-                      <Link
-                        to={"#"}
-                        onClick={followButton}
-                        className="btn-main"
-                      >
-                        {followButtonText}
-                      </Link>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          {totalFollowers} followers
+                        </div>
+                        <Link
+                          to={"#"}
+                          onClick={followButton}
+                          className="btn-main"
+                        >
+                          {followButtonText}
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
+                ) : (
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_avatar">
+                        <Skeleton
+                          width="150px"
+                          height="150px"
+                          borderRadius="50%"
+                        />
+
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            <Skeleton width="200px" />
+                            <span className="profile_username">
+                              <Skeleton width="100px" />
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              <Skeleton width="250px" />
+                            </span>
+                            <button id="btn_copy" title="Copy Text">
+                              Copy
+                            </button>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          <Skeleton width="150px" height="40px" />
+                        </div>
+                        <Link
+                          to={"#"}
+                          onClick={followButton}
+                          className="btn-main"
+                        >
+                          {followButtonText}
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </div>
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems authorData={authorData} isLoading={isLoading} />
                 </div>
               </div>
             </div>
